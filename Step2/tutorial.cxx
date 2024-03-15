3dd5335df500fc42b034f9a3642b1f46f371c834
Wang Ding <ggwdwhu@gmail.com>
2024-03-15 22:10:55 +0800

    accomplish step2

--- Step2/tutorial.cxx
+++ Step2/tutorial.cxx
@@ -4,6 +4,8 @@
 #include <string>
 
 // TODO 5: Include MathFunctions.h
+#include "MathFunctions.h"
+#include "MathFunctions/MathFunctions.h"
 #include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
@@ -20,9 +22,8 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // TODO 6: Replace sqrt with mathfunctions::sqrt
-
   // calculate square root
-  const double outputValue = sqrt(inputValue);
+  const double outputValue = mathfunctions::sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
